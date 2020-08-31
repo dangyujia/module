a7b3feb6f423771bf4a175f2ae8a9e11a9b1ab57
Administrator <963014657@qq.com>
2020-08-31 17:18:14 +0800

    首次提交

---
+++ a_module_ui/src/main/java/com/app/a_module_ui/AActivity.kt
@@ -0,0 +1,11 @@
+package com.app.a_module_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a)
+    }
+}
